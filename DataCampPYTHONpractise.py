8f29bc2cfb33f42683f93be1633b3968c07f74e3
Rohil Ahuja <rohilahuja3.com>
2025-05-23 23:41:12 +0530

    First Commit Module 1

---
+++ DataCampPYTHONpractise.py
@@ -0,0 +1,24 @@
+# x = [1,2,3]
+# print(type(x))
+# print(x)
+# n = len(x)
+# print(type(n))
+# print(n)
+
+# total = 0
+# for i in range(len(x)):
+#     total += x[i]
+#     print(total)
+#     print(type(total))
+
+# avg = int(total/n)
+# print(type(avg))
+# print(avg)
+
+pi = 3.148241
+print(type(pi))
+print('I love to eat', pi ,'!')
+
+pi_str = str(3.148241)
+print(type(pi_str))
+print('I love to eat ' + pi_str +'!')
